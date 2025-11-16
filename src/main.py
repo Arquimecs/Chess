32bd59aeea2111cd7d4baf85f78cd994516e4375
Arquimecs <bv2010cc@outlook.com>
2025-11-15 23:24:37 -0600

    some plotting

--- src/main.py
+++ src/main.py
@@ -1,3 +1,15 @@
 import matplotlib
 
-print(matplotlib._version)
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+print(matplotlib.__version__)
+print("Hello World!")
+
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints,'o--r')
+plt.show()
